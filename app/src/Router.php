8cb29eecbc715ba7e1ee79830c48322797da481d
Ivan Lyvin <ivan.lyvin@gmail.com>
2021-11-28 15:39:45 +0300

    #i-1 + routing

--- app/src/Router.php
+++ app/src/Router.php
@@ -2,7 +2,32 @@
 
 namespace App;
 
+use App\Exceptions\HttpException;
+
 class Router
 {
+    private array $routes = [
+        '/'      => 'Home',
+        '/task1' => 'FirstTask',
+        '/task2' => 'SecondTask',
+        '/task3' => 'Database'
+    ];
+
+    /**
+     * @return object
+     * @throws HttpException
+     * @throws \ReflectionException
+     */
+    public function getController(): object
+    {
+        foreach ($this->routes as $path => $className) {
+            if ($path === (string)$_SERVER['REQUEST_URI']) {
+               return (new \ReflectionClass('\\App\\Controllers\\'. $className . 'Controller'))
+                   ->newInstance();
+            }
+        }
+
+        throw new HttpException(404);
+    }
 
 }
