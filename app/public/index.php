019adac78cb03551e4edfefdabf0f059f0d3e48a
Ivan Lyvin <ivan.lyvin@gmail.com>
2021-11-28 15:39:23 +0300

    #i-1 + public php

--- app/public/index.php
+++ app/public/index.php
@@ -5,6 +5,13 @@ include dirname(__DIR__) . '/vendor/autoload.php';
 try {
     $app = \App\Kernel::getInstance();
     $app->run();
+} catch (\App\Exceptions\HttpException $exception) {
+    http_response_code($exception->getCode());
+    echo $exception->getMessage();
+
+    error_log(
+        $exception->getMessage()
+    );
 } catch (\Exception $exception) {
     error_log(
         $exception->getMessage()
